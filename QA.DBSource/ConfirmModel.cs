7618c911e077142ec5a2463c95ddcaa67dfd2408
A6228774 <A6228774@ulive.pccu.edu.tw>
2021-11-08 15:09:49 +0900

    ．加入ConfirmModel

---
+++ QA.DBSource/ConfirmModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QA.DBSource
+{
+    public class ConfirmModel
+    {
+        public int QID { get; set; }
+        public string Title { get; set; }
+        public string Type { get; set; }
+        public int CID { get; set; }
+        public String Answer { get; set; }
+    }
+}
